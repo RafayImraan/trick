8e81b40c09791601f00b730184942230e5a704f9
RafayImraan <aburafayyy@gmail.com>
2026-04-19 03:02:52 +0500

    fix: trust host and url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,11 @@ import Apple from 'next-auth/providers/apple';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from './prisma';
 
+const baseUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID || '',
